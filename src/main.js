3338baaacf65f8275c0d63a9f45ec337a6a7a83f
unknown <theerapattla@gmail.com>
2023-06-03 12:26:19 +0700

    portfolio commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import '../style/modren-normalize.css';
+import '../style/style.css';
+import '../style/font.css';
+import '../style/header.css';
+import '../style/sectionHero.css';
+import '../style/untils.css';
+import '../style/aboutme.css';
+import '../style/animation.css';
+import '../style/SKill.css';
+
